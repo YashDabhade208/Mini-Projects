788dd29b337ccd83d23fc8b4cbe29b44717c99b7
YashDabhade208 <yashdabhade208@gmail.com>
2024-09-02 20:18:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Input } from 'postcss';
+import Todo from './Todo';
+export default function App() {
+  
+
+    
+  return (
+    <>
+    <Todo/>
+
+
+    </>
+  )
+}
---
+++ src/Todo.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+
+
+
+const Todo = () => {
+
+    const [task, setTask] = useState([]);
+    const [newtask, setNewTask] = useState("");
+    const [completedtask,setcompletedtask]=useState(false);
+   
+    const handleinputchange = (e) => {
+        setNewTask(e.target.value)
+    }
+    const addTask = (e) => {
+        setTask([...task,newtask])
+
+    }
+    const moveup = (e) => {
+     const updatedTasks = [...task];
+     if(e>0){
+     [updatedTasks[e],updatedTasks[e-1]]=[updatedTasks[e-1],updatedTasks[e]]
+     setTask(updatedTasks);}
+    }
+    const movedown = (e) => {
+        const updatedTasks = [...task];
+        if(e<task.length){
+            [updatedTasks[e],updatedTasks[e+1]]=[updatedTasks[e+1],updatedTasks[e]]
+            setTask(updatedTasks);}
+       }
+   
+
+
+    function deleteTask(index) {
+        const updatedTasks = task.filter((task, i) => i !== index);
+      setTask(updatedTasks)
+    }
+    function taskstate(index){
+       
+    }
+
+    return (
+        <><div className='todo-container'>
+            <div>
+                <h1>TO DO LIST</h1>
+                <input onChange={handleinputchange} type="text" />
+                <button onClick={addTask}>Add Task</button>
+            </div>
+            <div >
+                <ol>
+                    {task.map((t, index) => (
+                        <li key={index}>
+                        <span style={{ textDecoration: task.completedtask ? 'line-through' : 'none' }}>
+                            {t}
+                        </span>
+                      
+                            <button onClick={()=>deleteTask(index)}>
+                                Delete
+                            </button>
+                            <button onClick={()=>moveup(index)}>
+                                Up
+                            </button>
+                            <button  onClick={()=>movedown(index)}>
+                                Down
+                            </button>
+                            <span  className=' Completed'> Completed</span>  <input type="checkbox" />
+                           
+                            
+                        </li>
+                    ))}
+                </ol>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Todo
